adcdf71fc97049b169a828f3393e9e222dee6699
AstashinE <eastashin@lineate.com>
2020-06-13 21:23:22 +0300

    added coroutines activity

--- app/src/main/java/com/astashin/petandroid/MainActivity.kt
+++ app/src/main/java/com/astashin/petandroid/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.astashin.petandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.astashin.petandroid.coroutines.CoroutinesActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.coroutines_btn).setOnClickListener {
+            startActivity(Intent(this, CoroutinesActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/astashin/petandroid/coroutines/CoroutinesActivity.kt
@@ -0,0 +1,13 @@
+package com.astashin.petandroid.coroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.astashin.petandroid.R
+
+class CoroutinesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coroutines)
+    }
+}
